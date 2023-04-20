f958ea84d81a824dbee9e2cef5b393566142abbe
Christian Bedo <thierry.moa.1410@gmail.com>
2023-04-20 16:06:58 -0400

    init project camunda client

---
+++ camunda-client/src/main/java/com/camunda/camundaclient/CamundaClientApplication.java
@@ -0,0 +1,13 @@
+package com.camunda.camundaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaClientApplication.class, args);
+	}
+
+}
---
+++ camunda-client/src/test/java/com/camunda/camundaclient/CamundaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.camundaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
